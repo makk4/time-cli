5ec2c758f2c342ac1d3a1bc53b5da5c494d65cd7
Manuel Klappacher <manuel.kl900@gmail.com>
2020-07-04 14:46:34 +0200

    switch to go modules

--- src/cmd/stop.go
+++ src/cmd/stop.go
@@ -17,27 +17,14 @@ package cmd
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/go-git/go-git"
-	"github.com/go-git/go-git/storage/memory"
 	"github.com/spf13/cobra"
 )
 
-//time-cli fuck you golang
-type Project struct {
-	startTime   string
-	endTime     string
-	id          string
-	name        string
-	tags        []string
-	description string
-}
-
-// startCmd represents the start command
-var startCmd = &cobra.Command{
-	Use:   "start",
-	Short: "Starts a new project",
+// stopCmd represents the stop command
+var stopCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -45,29 +32,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		var project Project
-		project.startTime = time.Now().String() //.Format("2006-01-02 15:04:05")
-		project.name = args[0]
-
-		fmt.Println(args[0], "startet @ ", project.startTime)
-		r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-			URL: "https://github.com/go-git/go-billy",
-		})
-		r.CommitObject()
+		fmt.Println("stop called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(stopCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// stopCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// stopCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ src/go.mod
@@ -0,0 +1,12 @@
+module time-cli
+
+go 1.14
+
+require (
+	github.com/go-git/go-git v4.7.0+incompatible
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.7.0
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+)
