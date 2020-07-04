f6c89384d9d554cc8501c22a9d195f5bd960700e
Manuel Klappacher <manuel.kl900@gmail.com>
2020-07-04 15:41:14 +0200

    add ksuid

--- src/cmd/start.go
+++ src/cmd/start.go
@@ -1,34 +1,19 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
-	"github.com/go-git/go-git"
-	"github.com/go-git/go-git/storage/memory"
+	"github.com/segmentio/ksuid"
 	"github.com/spf13/cobra"
 )
 
-//time-cli fuck you golang
+//Project test
 type Project struct {
-	startTime   string
-	endTime     string
-	id          string
+	startTime   time.Time
+	endTime     time.Time
+	id          ksuid.KSUID
 	name        string
 	tags        []string
 	description string
@@ -38,23 +23,24 @@ type Project struct {
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Starts a new project",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Starts a new project long 
+multi line descrp`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		var project Project
-		project.startTime = time.Now().String() //.Format("2006-01-02 15:04:05")
-		project.name = args[0]
 
-		fmt.Println(args[0], "startet @ ", project.startTime)
-		r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-			URL: "https://github.com/go-git/go-billy",
-		})
-		r.CommitObject()
+		project.startTime = time.Now()
+		project.name = args[0]
+		project.id = ksuid.New()
+
+		for i, s := range args {
+			if i > 0 && strings.HasPrefix(s, "+") {
+				project.tags = append(project.tags, string([]rune(s)[1:]))
+			}
+		}
+
+		fmt.Println(project.name, project.tags, "startet @ ", project.startTime.Format("2006-01-02 15:04:05"))
+
 	},
 }
 
--- src/go.mod
+++ src/go.mod
@@ -3,10 +3,14 @@ module time-cli
 go 1.14
 
 require (
-	github.com/go-git/go-git v4.7.0+incompatible
+	github.com/kr/text v0.2.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/segmentio/ksuid v1.0.3
 	github.com/spf13/cobra v1.0.0
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.0
-	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
